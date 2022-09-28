d47d5bad900606b7f5ad1e8022f8298f8bed9a06
Ykyki4 <jimmypantalones@gmail.com>
2022-09-28 16:37:21 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -8,12 +8,12 @@ def create_commendation(child_name, subject, year_of_study, group_letter):
 										group_letter=group_letter, 
 										subject__title=subject).order_by('-date').first()
 	Commendation.objects.create(
-					text=random.choice(COMMENDATION_EXAMPLES), 
-					created=last_lesson.date, 
-					schoolkid=child, 
-					subject=last_lesson.subject, 
-					teacher=last_lesson.teacher
- 	) 
+				text=random.choice(COMMENDATION_EXAMPLES),
+				created=last_lesson.date,
+				schoolkid=child,
+				subject=last_lesson.subject,
+				teacher=last_lesson.teache
+ 	)
 
 
 def remove_chastisements(schoolkid):
@@ -24,6 +24,5 @@ def remove_chastisements(schoolkid):
 def fix_marks(schoolkid):
 	child = Schoolkid.objects.get(full_name__contains=schoolkid)
 	bad_marks = Mark.objects.filter(schoolkid=child, 
-								points__in = [2, 3])
+									points__in = [2, 3])
 	bad_marks.update(points=5)
- 
